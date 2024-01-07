66b95db3e4c07e5994ed49e4e9fcee125cda54ea
Shubham2157 <1830193@kiit.ac.in>
2024-01-07 17:46:53 +0530

    remove folder creation

--- Backend/config/logger.js
+++ Backend/config/logger.js
@@ -1,45 +1,54 @@
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf } = format;
-const path = require('path');
-var fs = require('fs')
+//const path = require('path');
+//var fs = require('fs')
 
 const PORT = process.env.PORT || 3001
 const IP = process.env.IP || "127.0.0.1"
 
 const LOG_PATH = process.env.LOG_PATH || 'log';
-const dir = path.join(__dirname, '..' ,LOG_PATH)
-if (!fs.existsSync(dir)){
-  fs.mkdirSync(dir);
-}
+// const dir = path.join(__dirname, '..' ,LOG_PATH)
+// if (!fs.existsSync(dir)){
+//   fs.mkdirSync(dir);
+// }
+
+// var options = {
+//     file: {
+//       level: 'info',
+//       name: 'file.info',
+//       filename: path.join(__dirname, '..' ,LOG_PATH, 'app.log'),
+//       handleExceptions: true,
+//       json: true,
+//       maxsize: 5242880, // 5MB
+//       maxFiles: 100,
+//       colorize: true,
+//     },
+//     errorFile: {
+//       level: 'error',
+//       name: 'file.error',
+//       filename: path.join(__dirname, '..' ,LOG_PATH, 'error.log'),
+//       handleExceptions: true,
+//       json: true,
+//       maxsize: 5242880, // 5MB
+//       maxFiles: 100,
+//       colorize: true,
+//     },
+//     console: {
+//       level: 'debug',
+//       handleExceptions: true,
+//       json: false,
+//       colorize: true,
+//     },
+//   };
 
 var options = {
-    file: {
-      level: 'info',
-      name: 'file.info',
-      filename: path.join(__dirname, '..' ,LOG_PATH, 'app.log'),
-      handleExceptions: true,
-      json: true,
-      maxsize: 5242880, // 5MB
-      maxFiles: 100,
-      colorize: true,
-    },
-    errorFile: {
-      level: 'error',
-      name: 'file.error',
-      filename: path.join(__dirname, '..' ,LOG_PATH, 'error.log'),
-      handleExceptions: true,
-      json: true,
-      maxsize: 5242880, // 5MB
-      maxFiles: 100,
-      colorize: true,
-    },
-    console: {
-      level: 'debug',
-      handleExceptions: true,
-      json: false,
-      colorize: true,
-    },
-  };
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+  },
+};
 
   const myFormat = printf(({ level, message, label, timestamp }) => {
     return `${timestamp} [${label}] [${level.toUpperCase()}]: ${message}`;
